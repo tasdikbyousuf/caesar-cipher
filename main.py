eeeb3da9f195eaee19d18233e64ec120c38498d6
Tasdik <mtasdikyousuf@gmail.com>
2024-04-07 00:38:08 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+text = input("Enter your message: ")
+shift = int(input("Enter how many characters to be shifted: "))
+
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
+caesar(text, shift)
